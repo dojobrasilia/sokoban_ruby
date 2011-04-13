8d84d0b971753b8e8d2cd65b912a97e7e7747d3b
Bruno Pedroso <brunopedroso@gmail.com>
2011-04-13 17:56:56 -0300

    Refatoramos para usar matriz. quaaaaaase

--- Sokoban.rb
+++ Sokoban.rb
@@ -2,10 +2,17 @@ class Board
 	
 	def initialize(board)
 		@board = board
+		@board_m = board.split("\n")
 	end
 	
 	def current_board
 		@board
+		@board_m.join("\n")
+	end
+	
+	def left
+		@board_m[1][1]='x';
+		@board_m[1][2]=' ';
 	end
 	
 end
--- sokoban_spec.rb
+++ sokoban_spec.rb
@@ -14,4 +14,20 @@ describe Board do
 		
 	end
 
+
+	it "should go left" do
+	  board_st = "####\n" +
+						 	 "# x#\n" + 
+							 "####"
+		
+	  board_st_result = "####\n" +
+						 	        "#x #\n" + 
+							        "####"
+											
+		b = Board.new(board_st)
+		b.left
+		b.current_board.should == board_st_result
+	end
+	
+	
 end
