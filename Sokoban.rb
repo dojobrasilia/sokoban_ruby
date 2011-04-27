5c4d45994ae36d7865bd6999592f4d11f5ea9398
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2011-04-27 17:39:04 -0300

    Refatorando so ifs/unless do inferno

--- Sokoban.rb
+++ Sokoban.rb
@@ -31,12 +31,12 @@ class Board
 			next_row = @current[:row]+direction[:row]
 			next_col = @current[:col]+direction[:col]
 			
-			unless @board[next_row][next_col].chr == '#'
-			    if @board[next_row][next_col].chr == 'c'
-			        unless @board[next_row + direction[:row]][next_col + direction[:col]].chr  == '#' or  @board[next_row + direction[:row]][next_col + direction[:col]].chr  == 'c'
+			unless is_wall(next_row,next_col)
+			    if is_crate(next_row,next_col)
+			        unless is_wall(next_row + direction[:row],next_col + direction[:col]) or  is_crate(next_row + direction[:row],next_col + direction[:col])
 			            move_player(next_row,next_col)
 			            object_to_place = 'c'
-			            if (@board[next_row + direction[:row]][next_col + direction[:col]].chr == '.')
+			            if (is_goal(next_row + direction[:row],next_col + direction[:col]))
 			            	object_to_place = 'o'
 			            end
 			            @board[next_row + direction[:row]][next_col + direction[:col]]= object_to_place
@@ -46,6 +46,18 @@ class Board
 				end
 			end
 			
+		end
+		
+		def is_wall(row, col)
+			@board[row][col].chr == '#'
+		end
+		
+		def is_crate(row, col)
+			@board[row][col].chr == 'c'
+		end
+		
+		def is_goal(row, col)
+			@board[row][col].chr == '.'
 		end
 		
 		def find_player_position
